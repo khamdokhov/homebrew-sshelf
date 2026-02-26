28afc2b2f4ef97f55c1bceb0a68cc83917d4303f
khamdokhov <khamdokhov.a@inbox.ru>
2026-02-27 02:47:39 +0300

    Add version to formula

--- Formula/sshelf.rb
+++ Formula/sshelf.rb
@@ -5,6 +5,7 @@ class Sshelf < Formula
   desc "Terminal UI for managing SSH connections"
   homepage "https://gitverse.ru/khamdokhov/sshelf"
   url "https://gitverse.ru/api/repos/khamdokhov/sshelf/archive/a0dad1f341d15ad00ace19c708f4b591c80b760b.tar.gz"
+  version "0.1.0"
   sha256 "d8e5342876cee15ab6413c41d6bc014daf92e0a534d135e8dd544a1d76ca6a93"
   license "MIT"
 
