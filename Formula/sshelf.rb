0ce6dfc3ddbab28a23cae127a24e061d38cdc64d
khamdokhov <khamdokhov.a@inbox.ru>
2026-06-26 20:37:46 +0300

    Point formula at GitHub release v0.2.0

--- Formula/sshelf.rb
+++ Formula/sshelf.rb
@@ -3,10 +3,10 @@
 
 class Sshelf < Formula
   desc "Terminal UI for managing SSH connections"
-  homepage "https://gitverse.ru/khamdokhov/sshelf"
-  url "https://gitverse.ru/api/repos/khamdokhov/sshelf/archive/000432cd662261ea6d8000e84d2e73f4d2aeb8c3.tar.gz"
+  homepage "https://github.com/khamdokhov/sshelf"
+  url "https://github.com/khamdokhov/sshelf/archive/refs/tags/v0.2.0.tar.gz"
   version "0.2.0"
-  sha256 "9a5dba97dcdf624106435c6e53eea71b92a23aa63c73db86a7366664c8390b76"
+  sha256 "9e905ac61b4caeb80d69fa1ef961052610b31858834e61345de5da0188401423"
   license "MIT"
 
   depends_on "go" => :build
